6547ebf66488be68add52ab252a504135f9c1ec1
shougarianh <shougarianh@gmail.com>
2024-05-15 19:35:35 -0400

    Initial Commit

---
+++ StatsApp/StatsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatsAppApp.swift
+//  StatsApp
+//
+//  Created by Haik Shougarian on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct StatsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
